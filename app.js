eebc94169b4a03433f74ac6dd2e23b13bfd0456a
johon.lieghton@estudiantes.utec.edu.uy <johon.lieghton@estudiantes.utec.edu.uy>
2021-08-12 15:43:32 -0300

    cambiando a modo automatico

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Mundo-AUTOMATICO - Johon Lieghton'));
+app.get('/', (req, res) => res.send('Hola Mundo-AUTOMATICO POR FIN!!!! - Johon Lieghton'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
